a66bd63f677e8299e08bde3c539373ed92b3289a
hieusleep070610 <hieusleep070610@gmail.com>
2026-08-24 07:18:16 +0700

    Refactor research project router functions

--- app/routers/research_project.py
+++ app/routers/research_project.py
@@ -9,122 +9,58 @@ from services.research_projects import *
 
 router = APIRouter(prefix="/research-projects",tags=["Research Projects"])
 
-@router.post(
-    "",
-    response_model=ResearchProjectResponse
-)
-def create(
-    data: ResearchProjectCreate,
-    db: Session = Depends(get_db),
-    current_user: UserModel = Depends(get_current_user)
-):
-    return create_project(
-        data,
-        current_user,
-        db
-    )
-
-@router.post(
-    "",
-    response_model=ResearchProjectResponse
-)
-def create(
-    data: ResearchProjectCreate,
-    db: Session = Depends(get_db),
-    current_user: UserModel = Depends(get_current_user)
-):
-    return create_project(
-        data,
-        current_user,
-        db
-    )
+@router.post("",response_model=ResearchProjectResponse)
+def create_pj(
+    data: ResearchProjectCreate,db: Session = Depends(get_db),current_user: UserModel = Depends(get_current_user)):
+    return create_project(data,current_user,db)
 
 @router.get("")
-def get_all(
-    search: str = Query(None),
-    db: Session = Depends(get_db),
-    current_user: UserModel = Depends(get_current_user)
-):
-    return get_projects(
-        current_user,
-        db,
-        search
-    )
+def get_all_pj(search: str = Query(None),db: Session = Depends(get_db),current_user: UserModel = Depends(get_current_user)):
+    return get_projects(current_user,db,search)
 
-@router.get("/{project_id}")
-def get_detali(
-    project_id: int,
-    db: Session = Depends(get_db),
-    current_user: UserModel = Depends(get_current_user)
-):
-    return get_project(
-        project_id,
-        current_user,
-        db
-    )
+@router.get_detali_pj("/{project_id}")
+def get_detali(project_id: int,db: Session = Depends(get_db),current_user: UserModel = Depends(get_current_user)):
+    return get_project(project_id,current_user,db)
 
 @router.put("/{project_id}")
-def update(
+def update_pj(
     project_id: int,
     data: ResearchProjectUpdate,
     db: Session = Depends(get_db),
     current_user: UserModel = Depends(get_current_user)
 ):
-    return update_project(
-        project_id,
-        data,
-        current_user,
-        db
-    )
+    return update_project(project_id,data,current_user,db)
 
 @router.delete("/{project_id}")
-def delete(
+def delete_pj(
     project_id: int,
     db: Session = Depends(get_db),
     current_user: UserModel = Depends(get_current_user)
 ):
-    return delete_project(
-        project_id,
-        current_user,
-        db
-    )
+    return delete_project(project_id,current_user,db)
 
 @router.post("/{project_id}/members")
-def add(
+def add_members(
     project_id: int,
     data: MemberCreate,
     db: Session = Depends(get_db),
     current_user: UserModel = Depends(get_current_user)
 ):
-    return add_member(
-        project_id,
-        data.user_id,
-        current_user,
-        db
-    )
+    return add_member(project_id,data.user_id,current_user,db)
 
 @router.delete("/{project_id}/members/{user_id}")
-def remove(
+def remove_members(
     project_id: int,
     user_id: int,
     db: Session = Depends(get_db),
     current_user: UserModel = Depends(get_current_user)
 ):
-    return remove_member(
-        project_id,
-        user_id,
-        current_user,
-        db
-    )
+    return remove_member(project_id,user_id,current_user,db)
 
 @router.get("/{project_id}/members")
-def members(
+def get_members(
     project_id: int,
     db: Session = Depends(get_db),
     current_user: UserModel = Depends(get_current_user)
 ):
-    return get_members(
-        project_id,
-        current_user,
-        db
-    )
+    return get_members(project_id,current_user,db)
