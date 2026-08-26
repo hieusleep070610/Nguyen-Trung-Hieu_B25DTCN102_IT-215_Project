9b88a78015d8a3bcce55f00e91ad3bf89a67082f
hieusleep070610 <hieusleep070610@gmail.com>
2026-08-27 06:58:22 +0700

    Refactor exception handlers for better error messages

--- app/core/exception_handler.py
+++ app/core/exception_handler.py
@@ -1,42 +1,56 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException, status
 from fastapi.responses import JSONResponse
 from fastapi.exceptions import RequestValidationError
-from fastapi import HTTPException
 
 
 def register_exception_handlers(app: FastAPI):
 
     @app.exception_handler(HTTPException)
-    async def http_exception_handler(request: Request,exc: HTTPException):
+    async def http_exception_handler(
+        request: Request,
+        exc: HTTPException
+    ):
         return JSONResponse(
             status_code=exc.status_code,
             content={
+                "status_code": exc.status_code,
                 "message": exc.detail
             }
         )
 
     @app.exception_handler(RequestValidationError)
-    async def validation_exception_handler(request: Request,exc: RequestValidationError):
-        # Lấy lỗi
+    async def validation_exception_handler(
+        request: Request,
+        exc: RequestValidationError
+    ):
         error = exc.errors()[0]
 
         field = error["loc"][-1]
         msg = error["msg"]
 
         if msg == "Field required":
-            message = "Có Field bắt buộc phải nhập"
+            message = f"Trường '{field}' là bắt buộc"
+
         elif "Input should be greater than or equal to" in msg:
-            message = "Field không hợp lệ"
+            message = f"Giá trị của '{field}' không hợp lệ"
 
         elif "String should match pattern" in msg:
-            message = "Có Field nhập sai định dạng"
+            message = f"'{field}' không đúng định dạng"
+
+        elif "String should have at least" in msg:
+            message = f"'{field}' quá ngắn"
+
+        elif "String should have at most" in msg:
+            message = f"'{field}' quá dài"
 
         else:
             message = msg
 
         return JSONResponse(
-            status_code=422,
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
             content={
-                "message": message
+                "status_code": status.HTTP_422_UNPROCESSABLE_ENTITY,
+                "message": message,
+                "detail": exc.errors()
             }
         )
